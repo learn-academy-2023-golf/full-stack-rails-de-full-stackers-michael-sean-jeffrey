059a3d5c1ab6245ac1b6cafb388e3ab2a9280cd1
jeffguzi <jeffreystephano@gmail.com>
2023-11-28 15:37:51 -0800

    adds story 3

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,7 +2,12 @@ class BlogController < ApplicationController
     def index
         @blogs = Blog.all
     end
+
     def show
         @blog = Blog.find(params[:id])
+    end
+
+    def new 
+        @blog = Blog.new
     end
 end
